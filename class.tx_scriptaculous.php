f170fac10e980d69f530f899f8dd3a38025f6a8b
derjoerg <derjoerg@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-02-13 07:45:39 +0000

    Initial data upload of scriptaculous 1.6.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scriptaculous/trunk@4882 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_scriptaculous.php
@@ -0,0 +1,128 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *  
+ *  (c) 2006 Joerg Schoppet (joerg@schoppet.de)
+ *  All rights reserved
+ *
+ *  This script is part of the Typo3 project. The Typo3 project is 
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ * 
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/** 
+ * Plugin 'script.aculo.us' for the 'scriptaculous' extension.
+ *
+ * @author	Joerg Schoppet <joerg@schoppet.de>
+ */
+
+
+
+class tx_scriptaculous {
+	var $prefixId = 'tx_scriptaculous';		// Same as class name
+	var $scriptRelPath = 'class.tx_scriptaculous.php';	// Path to this script relative to the extension dir.
+	var $extKey = 'scriptaculous';	// The extension key.
+
+	/**
+	 * dummy
+	 */
+	function main($content,$conf)	{
+	}
+
+	/**
+	 * include the library and other data for page rendering
+	 * any configuration has to be done before with the set-functions
+	 */
+	function includeLib()	{
+
+		// add prototype to page content
+		if (!$GLOBALS['tx_scriptaculous']['tx_scriptaculous_prototype_inc']) {
+			// add scriptaculous to page header
+			$GLOBALS['TSFE']->additionalHeaderData['tx_scriptaculous_prototype_inc'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('scriptaculous') . 'lib/prototype.js"></script>';
+			$GLOBALS['tx_scriptaculous']['tx_scriptaculous_prototype_inc'] = TRUE;
+		}
+
+		// the config is parsed the following way
+		// 1. has the user set something within an application
+		// 2. is something defined by TS
+		// 3. use default values
+		if (!isset($GLOBALS['tx_scriptaculous']['prototype_only']))	{
+
+			if (isset($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_scriptaculous.']['prototype_only']))	{
+
+				if (strCaseCmp($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_scriptaculous.']['prototype_only'], 'true') == 0)	{
+					self::setPrototypeOnly(TRUE);
+				} else {
+					self::setPrototypeOnly(FALSE);
+				}
+
+			} else {
+				self::setPrototypeOnly();
+			}
+
+		}
+
+		if (!isset($GLOBALS['tx_scriptaculous']['functions']))	{
+
+			if (isset($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_scriptaculous.']['functions']))	{
+				$aFunctions = explode(',', $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_scriptaculous.']['functions']);
+			} else {
+				self::setFunctionsOnly();
+			}
+
+		}
+
+		// add scriptaculous to page content
+		if ($GLOBALS['tx_scriptaculous']['prototype_only'] === FALSE)	{
+
+			if (!$GLOBALS['tx_scriptaculous']['tx_scriptaculous_functions_inc']) {
+				$sLoad = '';
+
+				if (count($GLOBALS['tx_scriptaculous']['functions']) > 0)	{
+					$sLoad = '?load=' . implode(',', $GLOBALS['tx_scriptaculous']['functions']);
+				}
+
+				$GLOBALS['TSFE']->additionalHeaderData['tx_scriptaculous_functions_inc'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('scriptaculous') . 'src/scriptaculous.js' . $sLoad . '"></script>';
+				$GLOBALS['tx_scriptaculous']['tx_scriptaculous_functions_inc'] = TRUE;
+			}
+
+		}
+
+	}
+
+	/**
+	 * set value if only prototype should be included
+	 */
+	function setPrototypeOnly($bVar=FALSE)	{
+		$GLOBALS['tx_scriptaculous']['prototype_only'] = (bool)$bVar;
+	}
+	
+	/**
+	 * set value for special function load
+	 */
+	function setFunctionsOnly($aFunctions=array())	{
+
+		if (is_array($aFunctions)) {
+			$GLOBALS['tx_scriptaculous']['functions'] = $aFunctions;
+		}
+
+	}
+
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/scriptaculous/class.tx_scriptaculous.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/scriptaculous/class.tx_scriptaculous.php']);
+}
+
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "scriptaculous"
+#
+# Auto generated 13-02-2007 08:43
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'script.aculo.us',
+	'description' => 'JavaScript library for easy-to-use, cross-browser user interface',
+	'category' => 'fe',
+	'shy' => 1,
+	'version' => '1.6.5',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Joerg Schoppet',
+	'author_email' => 'joerg@schoppet.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:26:"class.tx_scriptaculous.php";s:4:"31b3";s:12:"ext_icon.gif";s:4:"28b7";s:24:"ext_typoscript_setup.txt";s:4:"ae13";s:13:"doc/CHANGELOG";s:4:"2ca0";s:15:"doc/MIT-LICENSE";s:4:"6444";s:10:"doc/README";s:4:"5c5d";s:14:"doc/manual.sxw";s:4:"76df";s:16:"lib/prototype.js";s:4:"6970";s:14:"src/builder.js";s:4:"fe88";s:15:"src/controls.js";s:4:"ea16";s:15:"src/dragdrop.js";s:4:"c0fc";s:14:"src/effects.js";s:4:"7f89";s:20:"src/scriptaculous.js";s:4:"6146";s:13:"src/slider.js";s:4:"ba4f";s:15:"src/unittest.js";s:4:"59d8";}',
+);
+
+?>
